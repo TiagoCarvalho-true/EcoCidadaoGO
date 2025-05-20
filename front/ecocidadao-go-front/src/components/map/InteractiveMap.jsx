52870e8cf7926bedb5588cb4476c6731ae080edf
TiagoCarvalho-true <carvalhost58@gmail.com>
2025-05-20 15:43:13 -0400

    estilização do componente map

--- front/ecocidadao-go-front/src/components/map/InteractiveMap.jsx
+++ front/ecocidadao-go-front/src/components/map/InteractiveMap.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
+import TrashPointPopup from './TrashPointPopup';
+import './InteractiveMap.css';
 
-// Corrige ícones quebrados no Leaflet com React
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
@@ -11,17 +12,37 @@ L.Icon.Default.mergeOptions({
   shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
 });
 
+const fakePoints = [
+  {
+    id: 1,
+    type: 'Plástico',
+    date: new Date(),
+    status: 'Pendente',
+    photo: 'https://via.placeholder.com/100',
+    position: [-15.7942, -47.8822],
+  },
+  {
+    id: 2,
+    type: 'Metal',
+    date: new Date(),
+    status: 'Recolhido',
+    photo: 'https://via.placeholder.com/100',
+    position: [-15.8000, -47.8800],
+  }
+];
+
 export default function InteractiveMap() {
   return (
-    <MapContainer center={[-15.7942, -47.8822]} zoom={13} style={{ height: '400px', width: '100%' }}>
+    <MapContainer center={[-3.119, -60.021]}
+ zoom={13} style={{ height: '400px', width: '50%' }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[-15.7942, -47.8822]}>
-        <Popup>
-          Ponto de coleta aqui!
-        </Popup>
-      </Marker>
+      {fakePoints.map(point => (
+        <Marker key={point.id} position={point.position}>
+          <TrashPointPopup point={point} />
+        </Marker>
+      ))}
     </MapContainer>
   );
 }
