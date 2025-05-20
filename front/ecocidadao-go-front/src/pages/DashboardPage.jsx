6a3c6af831f429b17145161d86974ae9b5486d6e
Bruno Marinho <brunomarinhosouza16@gmail.com>
2025-05-20 16:15:18 -0400

    Responsividade no dashboard/home e Alinhando os componentes no dashboard

--- front/ecocidadao-go-front/src/pages/DashboardPage.jsx
+++ front/ecocidadao-go-front/src/pages/DashboardPage.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './DashboardPage.css';
-import ErrorBoundary from '../components/common/ErrorBoundary';
 
 import Sidebar from '../components/common/Sidebar';
 import Header from '../components/common/Header';
+import ErrorBoundary from '../components/common/ErrorBoundary';
 
 import ScoreSummary from '../components/dashboard/ScoreSummary';
 import NotificationsList from '../components/dashboard/NotificationsList';
@@ -15,23 +15,29 @@ export default function DashboardPage() {
       <Sidebar />
 
       <div className="main-content">
-     {<Header/> }
+        <Header />
 
         <div className="dashboard-grid">
+
           <div className="left-panel">
-            <ScoreSummary />
+            <div className="panel-container">
+              <ScoreSummary />
+            </div>
           </div>
 
           <div className="right-panel">
-         <ErrorBoundary>
-  <NotificationsList />
-</ErrorBoundary>
+            <div className="panel-container">
+              <ErrorBoundary>
+                <NotificationsList />
+              </ErrorBoundary>
+            </div>
           </div>
 
           <div className="map-panel">
             <h2 className="map-title">Mapa Legal</h2>
             <InteractiveMap />
           </div>
+
         </div>
       </div>
     </div>
